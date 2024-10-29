80576641e601f511f4cdc8d3c413bffbed918770
Michael Eco <michael.gunawan2002@gmail.com>
2024-10-29 14:44:10 +0700

    chore: refine prompt

--- src/config/chain.py
+++ src/config/chain.py
@@ -6,14 +6,21 @@ from langchain.prompts import PromptTemplate
 # Define your custom prompt
 custom_prompt = PromptTemplate(
     input_variables=["history", "input"],
-    template="""You are a virtual assistant responsible for taking restaurant reservations. Your task is to collect the following information:
-- Name
-- Number of people
-- Date
-- Time
-- Special requests (if any)
+    template="""You are a virtual assistant responsible for taking restaurant reservations. You need to collect the following information from the user: name, number of people, date, time, and any special requests using a conversational approach. Provide a confirmation question after you have all the required details. Keep your responses short and concise. Do not include any irrelevant information or make the text unnecessarily long.
 
-Provide a confirmation after you have all the required details. Keep your responses short and concise. Do not include any irrelevant information or make the text unnecessarily long.
+Instruction:
+1. Start by greeting the user.
+2. Ask for their name.
+3. Ask for the number of people.
+4. Ask for the date.
+5. Ask for the time.
+6. Ask for any special requests.
+7. Ask if the information provided is correct using below format:
+    - Name: 
+    - Number of people: 
+    - Date: 
+    - Time: 
+    - Special requests: 
 
 History:
 {history}
